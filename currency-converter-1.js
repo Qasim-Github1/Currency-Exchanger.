b4f74d627b60f9976d2bd728f6018f3a983a421b
Muhammad Qasim <malikqasim91@gmail.com>
2024-08-03 02:07:51 +0500

    Fixed some bugs!

--- currency-converter-1.js
+++ currency-converter-1.js
@@ -1,12 +1,11 @@
 let countrySelection = document.querySelectorAll(".select"); 
  let exchangeButton = document.querySelector(".calculate-button");
-//let exchangeButton = document.getElementsByClassName('calculate-button');
 let amountValue = document.querySelector(".amount")
 const BASE_URL = "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies";
-  // const URL = ${BASE_URL}/${fromCurr.value.toLowerCase()}.json;
+
   const fromCurr = document.querySelector("#from")
   const toCurr = document.querySelector("#to")
-  let result = document.querySelector(".result")
+ let result = document.querySelector(".resultP")
 
 
     for (country of countrySelection)
@@ -31,27 +30,24 @@ const BASE_URL = "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/
         country.addEventListener("change",(e)=>{
                 updateFlag(e.target);
         })
+ // this is comment
 
         }
     const updateFlag = (element) => {
              let country = element.value;
-            // console.log(element);
-            // console.log(country);
+      
              let flag = countryList[country]
-            // console.log(flag);
+    
              let newImgSource = `https://flagsapi.com/${flag}/flat/64.png`
-            // console.log(newImgSource);
+        
      
          let img = element.parentElement.querySelector("img");
          img.src = newImgSource;
-         //console.log(img);
-             
+ 
     }
     exchangeButton.addEventListener("click", async (e)=>{
         e.preventDefault();
-        //console.log("clicked");
-        //console.log(amountValue.value);
-        //console.log(fromCurr.value , toCurr.value);
+     
         const URL = `${BASE_URL}/${fromCurr.value.toLowerCase()}/${toCurr.value.toLowerCase()}.json`;
         let response = await fetch(URL);
         console.log(response);
